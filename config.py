69bc38f0f98fddbdc59986a685b81ce5404aee14
nadhixd <raam43927@gmail.com>
2025-01-27 22:16:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Shivmis/cuberon",
+    "https://github.com/nadhixd",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
